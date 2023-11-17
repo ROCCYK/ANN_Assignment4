b7f4a2b113b183312ac68b9f6b76f8314d0e4ae5
Rhichard Koh <82534598+ROCCYK@users.noreply.github.com>
2023-11-16 20:32:59 -0500

    commit

--- train.py
+++ train.py
@@ -93,14 +93,14 @@ if __name__ == "__main__":
         lin_reg = LinearRegression()
 
         # Use mini batches for memory efficiency and faster convergence
-        batch_size = 32
+        batch_size = 64
         train_dataset = tf.data.Dataset.from_tensor_slices((x_train_norm, y_train_norm))
         train_dataset = train_dataset.shuffle(buffer_size=x_train.shape[0]).batch(batch_size)
         test_dataset = tf.data.Dataset.from_tensor_slices((x_test_norm, y_test_norm))
         test_dataset = test_dataset.shuffle(buffer_size=x_test.shape[0]).batch(batch_size)
 
         # Set training parameters
-        epochs = 100
+        epochs = 200
         learning_rate = 0.01
         train_losses, test_losses = [], []
 
